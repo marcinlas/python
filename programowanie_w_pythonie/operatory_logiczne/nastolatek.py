848ed15ec9ec1d59d44ca365ae56edd64db0dd45
Javier <marcin_las@op.pl>
2025-04-10 22:03:24 +0200

    Update nastolatek.py

--- programowanie_w_pythonie/operatory_logiczne/nastolatek.py
+++ programowanie_w_pythonie/operatory_logiczne/nastolatek.py
@@ -1,9 +1,13 @@
 '''
-Przekształć podaną funkcję teenager(age) w taki sposób, żeby poprawnie oceniała,
-czy podana wartość age odpowiada wieku nastolatka,
-czyli leży w przedziale od 13 do 19 (włącznie).
-Zamiast or trzeba użyć innego operatora logicznego.
+Przekształć podaną funkcję teenager(age) w taki sposób, żeby poprawnie oceniała, czy podana wartość age odpowiada
+wieku nastolatka, czyli leży w przedziale od 13 do 19 (włącznie). Zamiast or trzeba użyć innego operatora logicznego.
+
+KOD DO ZMIANY:
+def teenager(age):
+  return age > 9 or age <= 20
 '''
 
 def teenager(age):
-    return age > 10 or age <= 20
+    return age > 12 and age <= 19
+
+# https://www.umiemyinformatyke.pl/interaktywny-python-operatory-logiczne/76
