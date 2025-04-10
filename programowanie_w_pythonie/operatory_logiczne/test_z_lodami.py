ed85b6d585aa8f9c4d363f2c76cc695991f54e87
Javier <marcin_las@op.pl>
2025-04-10 22:21:42 +0200

    Create test_z_lodami.py

---
+++ programowanie_w_pythonie/operatory_logiczne/test_z_lodami.py
@@ -0,0 +1,14 @@
+'''
+Józek chce kupić lody, ale nie wie, czy starczy mu pieniędzy. Napisz funkcję icecream_test(money, price), która dla
+podanej kwoty (money) i ceny lodów (price) zdecyduje, czy może Józek może kupić lody.
+
+KOD DO ZMIANY:
+def icecream_test(money, price):
+    return True
+'''
+
+def icecream_test(money, price):
+    return money >= price
+
+# testowane kodem --> print(icecream_test(100, 40))
+# źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-operatory-logiczne/63
