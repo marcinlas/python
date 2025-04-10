df212420ac833d51921601f371580da84d79cb5e
Javier <marcin_las@op.pl>
2025-04-10 23:51:01 +0200

    Update wielka_parzysta.py

--- programowanie_w_pythonie/operatory_logiczne/wielka_parzysta.py
+++ programowanie_w_pythonie/operatory_logiczne/wielka_parzysta.py
@@ -7,15 +7,16 @@ def big_even(a, b):
 '''
 
 def big_even(a, b):
-    if a>b and a%2 == 0:
-        if b>a and b%2 == 0:
-            return False
+    if a>b and a%2 == 0 or b>a and b%2 == 0:
+        return not False
     else:
-        return True
+        return False
 
 # testowane przypadki:
 print(big_even(2, 5))
 print(big_even(2, 4))
-
+print(big_even(7, 3))
+print(big_even(8, 6))
+print(big_even(8, 5))
 
 # źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-operatory-logiczne/60
