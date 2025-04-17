4747772f495697afdc497af8379ea783550b1739
Javier <marcin_las@op.pl>
2025-04-17 18:24:54 +0200

    Create magiczny_test.py

---
+++ programowanie_w_pythonie/operatory_logiczne/magiczny_test.py
@@ -0,0 +1,19 @@
+''' Czarownica chce przygotować eliksir miłości. Do przygotowania potrzebuje
+5 łez feniksa i róg jednorożca. Jeżeli nie ma rogu jednorożca, może użyć 3 ogonków
+fioletowych prosiaczków. W takim przypadku wystarczą jej tylko 3 łzy feniksa.
+Napisz funkcję magic_test(tears, horns, tails), która dla podanej liczby łez (tears),
+rogów (horns) i ogonków (tails) zdecyduje, czy czarownica może przygotować eliksir.
+
+KOD DO ZMIANY:
+def magic_test(tears, horns, tails):
+    return False
+'''
+
+def magic_test(tears, horns, tails):
+    return False
+
+
+# testowane przypadki:
+print(magic_test(4, 3, 3))
+
+# źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-operatory-logiczne/64
