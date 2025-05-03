0c5a6d81189eff186fead90aa8f393ec610fc3e3
Javier <marcin_las@op.pl>
2025-05-03 21:59:15 +0200

    Create wypis_liczb.py

---
+++ programowanie_w_pythonie/petla_for/wypis_liczb.py
@@ -0,0 +1,15 @@
+''' Napisz funkcję numbers(n), która wypisze pierwszych n liczb naturalnych.
+
+KOD DO ZMIANY:
+def numbers(n):
+    print(n)
+'''
+
+def numbers(n):
+    for i in range(1,n+1):
+        print(i)
+
+# testowane przypadki:
+numbers(10)
+
+# źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-for/1
