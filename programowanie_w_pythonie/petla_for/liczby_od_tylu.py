79e0708d1d70aa266d90b44a218594e96ab9fcda
Javier <marcin_las@op.pl>
2025-05-03 21:21:52 +0200

    Create liczby_od_tylu.py

---
+++ programowanie_w_pythonie/petla_for/liczby_od_tylu.py
@@ -0,0 +1,17 @@
+''' Napisz funkcję reverse_numbers(n), która wypisze liczby od 1 do n od tyłu.
+
+KOD DO ZMIANY:
+def reverse_numbers(n):
+    for i in range(n):
+        print(i)
+'''
+
+def reverse_numbers(n):
+    for i in range(n):
+        print(i)
+
+
+# testowane przypadki:
+reverse_numbers(8)
+
+# źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-for/56
