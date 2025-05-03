842aa6078be512e88afa0b256821337afe00748d
Javier <marcin_las@op.pl>
2025-05-03 22:05:02 +0200

    Create interwal_liczb.py

---
+++ programowanie_w_pythonie/petla_for/interwal_liczb.py
@@ -0,0 +1,14 @@
+''' Napisz funkcję interval(a, b), która wypisze liczby od a do b.
+
+KOD DO ZMIANY:
+def interval(a, b):
+    print()
+'''
+
+def interval(a, b):
+    print()
+
+# testowane przypadki:
+interval(8, 13)
+
+# źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-for/99
