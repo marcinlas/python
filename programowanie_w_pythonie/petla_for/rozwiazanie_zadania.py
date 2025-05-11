6ee8085312012ef66c6c5ba845a04e72e1a6e4ce
Javier <marcin_las@op.pl>
2025-05-11 21:20:09 +0200

    Create rozwiazanie_zadania.py

---
+++ programowanie_w_pythonie/petla_for/rozwiazanie_zadania.py
@@ -0,0 +1,15 @@
+''' Napisz funkcję even_numbers(n), która wypisze n liczb parzystych (począwszy od dwójki).
+
+KOD DO ZMIANY:
+def even_numbers(n):
+    print(2)
+
+'''
+
+    for i in range(2, n)
+        print(2)
+
+# testowane przypadki:
+even_numbers(6)
+
+# źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-for/57
