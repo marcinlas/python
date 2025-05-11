04d5023d7b7722bac03944a740eb61489f4a3a80
Javier <marcin_las@op.pl>
2025-05-11 16:12:44 +0200

    Create wypis_wielokrotnosci.py

---
+++ programowanie_w_pythonie/petla_for/wypis_wielokrotnosci.py
@@ -0,0 +1,16 @@
+''' Napisz funkcję multiples(k, n), która wypisze n pierwszych wielokrotności liczby k.
+
+
+KOD DO ZMIANY:
+def multiples(k, n):
+    print()
+'''
+
+def multiples(k, n):
+    print()
+
+
+# testowane przypadki:
+multiples(4, 8)
+
+# źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-for/100
