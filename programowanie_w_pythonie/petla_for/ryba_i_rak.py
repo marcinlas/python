1332bc0fd523876cd8dd26a064d491a5b9a3a79c
Javier <marcin_las@op.pl>
2025-06-05 18:37:48 +0200

    Create ryba_i_rak.py

---
+++ programowanie_w_pythonie/petla_for/ryba_i_rak.py
@@ -0,0 +1,16 @@
+''' Napisz funkcję animals(n), która wypisze n powtórzeń słów ryba, które są przeplatane słowem rak.
+
+KOD DO ZMIANY:
+def animals(n):
+    print("ryba")
+'''
+
+def animals(n):
+    for i in range(n):
+        print("ryba")
+        print("rak")
+
+# testowane przypadki:
+animals(3)
+
+# źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-for/98
