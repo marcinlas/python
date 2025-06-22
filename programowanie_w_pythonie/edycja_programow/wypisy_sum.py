cd288ec3957506c709446ea63d600429e51a532c
Javier <marcin_las@op.pl>
2025-06-22 13:05:48 +0200

    Update wypisy_sum.py

--- programowanie_w_pythonie/edycja_programow/wypisy_sum.py
+++ programowanie_w_pythonie/edycja_programow/wypisy_sum.py
@@ -9,11 +9,17 @@ def print_sums(n):
 '''
 
 def print_sums(n):
-    for i in range(n):
-        print(n, "=", i, "+", i)
+        for i in range(n):
+            if n - i - 1 > 0:
+                print(n, "=", i+1, "+", n-i-1)
+
+
+
+
+
 
 
 # testowane przypadki:
-print_sums(13)
+print_sums(9)
 
 # źródło --> https://www.umiemyinformatyke.pl/interaktywne-python-edycja-programow/45
