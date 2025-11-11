4fbb7bffde4328105ea0770b04475a9374cf783e
Javier <marcin_las@op.pl>
2025-11-11 14:25:51 +0100

    Create trojkowy_foobar.py

---
+++ programowanie_w_pythonie/petla_for_z_warunkiem_zagniezdzonym/trojkowy_foobar.py
@@ -0,0 +1,22 @@
+''' Józek oznacza liczby podzielne przez trzy słowem foo. Pozostałe liczby oznacza słowem bar.
+Napisz funkcję foobar3(n), która wypisze pierwszych n liczb naturalnych wraz z ich oznaczeniem.
+
+KOD DO ZMIANY:
+def foobar3(n):
+    print("foobar")
+
+'''
+
+def stars(n):
+    for i in range(n):
+        for i in range(i<=5):
+            print("*", end="")
+        if i == 5:
+            print("|", end="")
+    print()
+
+# testowane przypadki:
+foobar3(10)
+
+
+# źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-for-if/67?source=explicitExercise
