81b1bc466dcd9367f630eca142f9e14ccc09da26
Javier <marcin_las@op.pl>
2026-01-27 20:29:52 +0100

    Create wielokrotne_dzielenie_na_pol.py

---
+++ programowanie_w_pythonie/petla_while/wielokrotne_dzielenie_na_pol.py
@@ -0,0 +1,17 @@
+''' Napisz funkcję repeated_division(n), która wielokrotnie wypisuje i dzieli podaną dodatnią liczbę n na pół,
+dopóki jest większa od zera (patrz przykład). Użyj dzielenia całkowitego,
+które w Pythonie zapisuje się za pomocą dwóch ukośników (//)..
+
+KOD DO ZMIANY:
+def repeated_division(n):
+    print(n)
+'''
+
+def repeated_division(n):
+    print(n)
+
+# testowane przypadki:
+repeated_division(89)
+
+
+# źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-while/116
