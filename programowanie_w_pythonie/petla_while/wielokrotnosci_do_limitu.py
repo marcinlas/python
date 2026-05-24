c0306aee28d769bb5f5eb9c419dc25d8d0a787f2
Javier <marcin_las@op.pl>
2026-05-24 20:57:49 +0200

    Create wielokrotnosci_do_limitu.py

---
+++ programowanie_w_pythonie/petla_while/wielokrotnosci_do_limitu.py
@@ -0,0 +1,21 @@
+''' Napisz funkcję multiples(n, limit),
+która wypisze wielokrotności liczby n mniejsze niż limit.
+
+
+KOD DO ZMIANY:
+def multiples(n, limit):
+    print(n)
+
+'''
+
+def count_zeros(n):
+    # Zamiana na tekst i policzenie ile razy występuje znak "0"
+    ileJestZer = str(n).count("0")
+    print(ileJestZer)
+
+
+# testowane przypadki:
+multiples(7, 85)
+
+
+# źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-while/117
