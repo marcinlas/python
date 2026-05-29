181946153b8035dd98a5b0b1faa13cc8b675b3fb
Javier <marcin_las@op.pl>
2026-05-29 21:23:11 +0200

    Create potegi_w_przedziale.py

---
+++ programowanie_w_pythonie/petla_while/potegi_w_przedziale.py
@@ -0,0 +1,25 @@
+''' Marcin ma mniej drewna niż kamieni.
+Za jednym razem wydobywa 2 drewna i 1 kamień. Będzie wydobywał,
+dopóki nie będzie miał tyle drewna, ile kamieni.
+Napisz funkcję materials(d, k),
+która dla podanej liczby drewna i kamieni wypisze przebieg
+wydobycia (patrz przykład).
+
+
+def powers(n, a, b):
+    print(n)
+
+
+'''
+
+def powers(n, a, b):
+    print(n)
+
+
+
+# testowane przypadki:
+powers(2, 50, 1000)
+
+
+
+# źródło --> https://www.umiemyinformatyke.pl/interaktywny-python-while/136
